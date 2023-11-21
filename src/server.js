89206afade3885f37a3e926563ea228beae754d3
leif.le <leif.le@avepoint.com>
2023-11-21 10:58:23 +0700

    config env

--- src/server.js
+++ src/server.js
@@ -1,23 +1,25 @@
 const express = require("express"); //commonjs
 const path = require("path");
+require("dotenv").config();
 //import express from "express"; //es modules
 
 const app = express(); //app express
-const port = 8081; //port
+const port = process.env.PORT || 8888; //port
+const hostname = process.env.HOST_NAME;
 
 //config template engine
-app.set("views", path.join(__dirname, "./views/"));
+app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 
 //routes declarations
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World w server");
 });
 
 app.get("/about", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
